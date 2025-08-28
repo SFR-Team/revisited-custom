3adbd995c18c3bd4b3c74dd525d3ffb5ea50452f
Ashrindy <honzula1000@gmail.com>
2025-08-28 21:25:34 +0200

    damage boost fixes

--- src/player/StateHooks.h
+++ src/player/StateHooks.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void DamageWhileBoost(app::player::PlayerHsmContext* ctx) {
+void DamageWhileBoost(app::player::PlayerHsmContext* ctx, bool boost = false) {
 	static bool wasDamageSet = false;
 
 	if (auto* collision = ctx->gocPlayerHsm->statePluginManager->GetPlugin<app::player::StatePluginCollision>()) {
@@ -8,7 +8,8 @@ void DamageWhileBoost(app::player::PlayerHsmContext* ctx) {
 		if (auto* status = ctx->blackboardStatus) {
 			bool boosting = status->GetStateFlag(app::player::BlackboardStatus::StateFlag::BOOST);
 			bool powerBoost = status->GetWorldFlag(app::player::BlackboardStatus::WorldFlag::POWER_BOOST);
-			if (boosting &&
+			if (boost || 
+				boosting &&
 				powerBoost &&
 				!wasDamageSet)
 			{
@@ -26,24 +27,24 @@ void DamageWhileBoost(app::player::PlayerHsmContext* ctx) {
 	}
 }
 
-HOOK(bool, __fastcall, StateRunUpdateAsync, 0x140964B00, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
-	auto res = originalStateRunUpdateAsync(self, ctx);
+HOOK(bool, __fastcall, StateRunStep, 0x140964B00, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
+	auto res = originalStateRunStep(self, ctx);
 
 	DamageWhileBoost(ctx);
 
 	return res;
 }
 
-HOOK(bool, __fastcall, StateGrindUpdateAsync, 0x14B618E60, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
-	auto res = originalStateGrindUpdateAsync(self, ctx);
+HOOK(bool, __fastcall, StateGrindStep, 0x14B618E60, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
+	auto res = originalStateGrindStep(self, ctx);
 
 	DamageWhileBoost(ctx);
 
 	return res;
 }
 
-HOOK(bool, __fastcall, StateAirBoostUpdateAsync, 0x14B577D60, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
-	auto res = originalStateAirBoostUpdateAsync(self, ctx);
+HOOK(bool, __fastcall, StateAirBoostStep, 0x14B577D60, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
+	auto res = originalStateAirBoostStep(self, ctx);
 
 	DamageWhileBoost(ctx);
 
@@ -66,21 +67,44 @@ HOOK(bool, __fastcall, StateRightStepRunUpdate, 0x140965B00, app::player::Player
 	return res;
 }
 
-HOOK(bool, __fastcall, StateDriftDashUpdateAsync, 0x1409566D0, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
-	auto res = originalStateDriftDashUpdateAsync(self, ctx);
+HOOK(bool, __fastcall, StateDriftDashStep, 0x1409566D0, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
+	auto res = originalStateDriftDashStep(self, ctx);
 
-	DamageWhileBoost(ctx);
+	DamageWhileBoost(ctx, true);
 
 	return res;
 }
 
+HOOK(void, __fastcall, StateDriftDashLeave, 0x140956450, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx, int nextState) {
+	originalStateDriftDashLeave(self, ctx, nextState);
+
+	DamageWhileBoost(ctx, false);
+}
+
+HOOK(bool, __fastcall, StateDropDashStep, 0x140956D20, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx) {
+	auto res = originalStateDropDashStep(self, ctx);
+
+	DamageWhileBoost(ctx, true);
+
+	return res;
+}
+
+HOOK(void, __fastcall, StateDropDashLeave, 0x140956CA0, app::player::PlayerStateBase* self, app::player::PlayerHsmContext* ctx, int nextState) {
+	originalStateDropDashLeave(self, ctx, nextState);
+
+	DamageWhileBoost(ctx, false);
+}
+
 namespace revisited::player {
 	void bootstrapStates() {
-		INSTALL_HOOK(StateRunUpdateAsync);
-		INSTALL_HOOK(StateGrindUpdateAsync);
-		INSTALL_HOOK(StateAirBoostUpdateAsync);
+		INSTALL_HOOK(StateRunStep);
+		INSTALL_HOOK(StateGrindStep);
+		INSTALL_HOOK(StateAirBoostStep);
 		INSTALL_HOOK(StateLeftStepRunUpdate);
 		INSTALL_HOOK(StateRightStepRunUpdate);
-		INSTALL_HOOK(StateDriftDashUpdateAsync);
+		INSTALL_HOOK(StateDriftDashStep);
+		INSTALL_HOOK(StateDriftDashLeave);
+		INSTALL_HOOK(StateDropDashStep);
+		INSTALL_HOOK(StateDropDashLeave);
 	}
 }
