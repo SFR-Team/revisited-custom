629f48628fefcf269ef2f1c1795fba5e587b2bae
Ashrindy <honzula1000@gmail.com>
2026-01-07 17:19:49 +0100

    Enable Cyloop in Cyberspace

--- src/player/bootstrap.h
+++ src/player/bootstrap.h
@@ -39,6 +39,29 @@ HOOK(uint64_t, __fastcall, MessageHandler, 0x14091CF80, app::player::PlayerHsmCo
 	return originalMessageHandler(self, message);
 }
 
+// Enable Cyloop in Cyberspace - TODO: Fix crash when trying to spawn ObjSequenceItem
+
+// TODO: Map StatePluginCyloop
+FUNCTION_PTR(app::player::StatePlugin<app::player::PlayerHsmContext>*, __fastcall, StatePluginCyloopCtor, 0x1409A4080, void*, csl::fnd::IAllocator*);
+
+HOOK(void, __fastcall, InitializePlayer, 0x14088AC20, app::player::Player* self) {
+	originalInitializePlayer(self);
+	auto& playerHsm = self->GetComponent<app::player::GOCPlayerHsm>()->statePluginManager;
+	auto* plugin = StatePluginCyloopCtor(playerHsm->GetAllocator()->Alloc(216, 8), playerHsm->GetAllocator());
+	plugin->context = playerHsm->context;
+	playerHsm->AddPlugin(plugin);
+}
+
+// This disables any kind of ObjSkillPoint spawn using a special function, if the player is in CyberSpace
+// It's a fix for Cyber Cyloop
+HOOK(void, __fastcall, sub_14049AEB0, 0x14049AEB0, int64_t self, int64_t a2, float a3, char a4) {
+	if (auto* stageData = hh::game::GameManager::GetInstance()->GetService<app::level::LevelInfo>()->GetStageData())
+		if (stageData->GetAttributeFlag(app::level::StageData::AttributeFlags::CYBER))
+			return;
+
+	originalsub_14049AEB0(self, a2, a3, a4);
+}
+
 namespace revisited::player {
 	void bootstrap() {
 		auto* allocator = hh::fnd::MemoryRouter::GetModuleAllocator();
@@ -49,6 +72,8 @@ namespace revisited::player {
 
 		INSTALL_HOOK(PlayerAddCallback);
 		INSTALL_HOOK(MessageHandler);
+		INSTALL_HOOK(InitializePlayer);
+		INSTALL_HOOK(sub_14049AEB0);
 
 		bootstrapStates();
 	}
