300b64527e7a463d2872202545725f270ecda152
Ashrindy <honzula1000@gmail.com>
2026-08-25 10:40:51 +0200

    fix

--- src/player/DimensionVolumeFix.h
+++ src/player/DimensionVolumeFix.h
@@ -3,7 +3,7 @@
 HOOK(void, __fastcall, sub_140915530, 0x140915530, app::player::PlayerHsmContext* self) {
 	originalsub_140915530(self);
 
-	if (auto* levelInfo = self->GetGameManager()->GetService<app::level::LevelInfo>()) {
+	if (auto* levelInfo = self->GetPlayerObject()->gameManager->GetService<app::level::LevelInfo>()) {
 		if (levelInfo->GetStageData()->GetAttributeFlag(app::level::StageData::AttributeFlags::CYBER)) {
 			auto* gocPlayerParam = self->GetPlayerObject()->GetComponent<app::player::GOCPlayerParameter>();
 			gocPlayerParam->SetMode(app::player::GOCPlayerParameter::Mode::CYBERSPACE_FORWARD_VIEW);
